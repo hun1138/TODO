b16465d7ee9c9cf5aacea7aaea05c83d7e338904
hun <qwsdxc313@gmail.com>
2018-11-02 15:24:13 +0900

    add form, detail.html

--- TODO/todoList/admin.py
+++ TODO/todoList/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from .models import Todo
+
 # Register your models here.
+
+class TodoAdmin(admin.ModelAdmin):
+    field = ['todo_title', 'todo_info']
+
+admin.site.register(Todo, TodoAdmin)
--- TODO/todoList/views.py
+++ TODO/todoList/views.py
@@ -1,5 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from .models import Todo
+from .forms import TodoForm
 
 # Create your views here.
 def index(request):
-    pass
+    if request.method == 'POST':
+        todoForm = TodoForm(request.POST)
+        if todoForm.is_valid():
+            todoForm.save()
+    else:
+        todoForm = TodoForm()
+    todoList = Todo.objects.all()
+    todoLen = len(todoList)
+    context = {
+        'todoForm':todoForm,
+        'todoList':todoList,
+        'todoLen':todoLen,
+    }
+    return render(request, 'index.html', context)
+
+def detail(request, todo_id):
+    todo = get_object_or_404(Todo, pk=todo_id)
+    context = {
+        'todo':todo,
+    }
+    return render(request, 'detail.html', context)
