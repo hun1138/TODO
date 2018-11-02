aa9913da1c8f330df5ecd55fc69717f7eed1cc0e
hun <qwsdxc313@gmail.com>
2018-11-02 15:54:39 +0900

    update form

--- TODO/todoList/forms.py
+++ TODO/todoList/forms.py
@@ -1,7 +1,15 @@
 from django import forms
 from .models import Todo
 
+
 class TodoForm(forms.ModelForm):
     class Meta:
         model = Todo
         fields = ['todo_title', 'todo_info']
+'''
+class TodoForm(forms.Form):
+    todo_title = forms.CharField(label='title', max_length=20)
+    todo_info = forms.CharField(label='info', widget=forms.Textarea)
+    #todo_due_date = forms.DateTimeField(widget=forms.DateInput)
+    todo_due_date = forms.DateField(widget=forms.SelectDateWidget)
+'''
