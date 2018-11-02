1d5ad145b7aa4182561769e6b792570bddda2575
hun <qwsdxc313@gmail.com>
2018-11-02 14:40:56 +0900

    init project

--- TODO/todoList/models.py
+++ TODO/todoList/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Todo(models.Model):
+    todo_title = models.CharField(max_length=20)
+    todo_info = models.TextField()
+    todo_due_date = models.DateTimeField(null=True)
+    todo_is_complete = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
